f45fecd747efccef9a47192d800e8c9c7beaecd5
Dev01 <Dev01@WIN-KIVPNI89GEV>
2023-08-12 03:05:54 +0300

    WIP

--- src/assets/lib/them/assets/js/loader.js
+++ src/assets/lib/them/assets/js/loader.js
@@ -1,4 +1,6 @@
-window.addEventListener("load", function(){
-    var load_screen = document.getElementById("load_screen");
-    document.body.removeChild(load_screen);
-});
+window.onload = function() {
+  var load_screen = document.getElementById("load_screen");
+  if (load_screen) {
+      document.body.removeChild(load_screen);
+  }
+};
